0ee94d26eda1e2fc3a808b7b1dcc5ccbef35aff5
Septio9 <septiogultom9@gmail.com>
2023-03-08 23:07:33 +0700

    week1

---
+++ Segitiga.java
@@ -0,0 +1,9 @@
+public class Segitiga{
+    public static void main(String[] args) {
+        double alas = 6;
+        double tinggi = 8;
+        double luas = (alas * tinggi) / 2;
+
+        System.out.println("Luas segitiga siku-siku dengan alas " + alas + " dan tinggi " + tinggi + " adalah " + luas);
+    }
+}
---
+++ biodata.java
@@ -0,0 +1,10 @@
+public class biodata{
+    public static void main(String[] args){
+
+    String nama="Gabriel Septio";
+    System.out.println("Halo, nama saya adalah "+nama);
+    System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+    System.out.println("Saya sedang menulis program java");
+    
+    }
+}
